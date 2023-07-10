5b7848332bb21ab84e00bd24779f7d02ecb3e26c
eskander1994 <121143092+eskander1994@users.noreply.github.com>
2023-07-09 21:53:12 -0500

    utils page

---
+++ src/test/java/utils/BrowserUtils.java
@@ -0,0 +1,91 @@
+package utils;
+
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Set;
+
+public class BrowserUtils {
+    public static String getText(WebElement element){
+        return element.getText().trim();
+    }
+    public static void selectBy(WebElement location ,String value , String methodName){
+        Select select = new Select(location);
+        switch (methodName){
+            case "text":
+                select.selectByVisibleText(value);
+                break;
+            case "value":
+                select.selectByValue(value);
+                break;
+            case "index":
+                select.selectByIndex(Integer.parseInt(value));
+                break;
+            default:
+                System.out.println("your method name is not correct");
+                Assert.assertTrue(false);
+        }
+    }
+    public static String  getTitleWithJS(WebDriver driver){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js.executeScript("return document.title").toString().trim();
+
+    }
+    public static void clickWithJS(WebDriver driver,WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click()",element);
+    }
+public static void scrollWithJS(WebDriver driver,WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true)",element);
+}
+
+public static void  switchById(WebDriver driver){
+    String mainPage = driver.getWindowHandle();
+    Set<String> allPages = driver.getWindowHandles();
+    for(String page:allPages){
+        if(!page.equals(mainPage)){
+            driver.switchTo().window(page);
+            break;
+        }
+    }
+}
+public static void switchByTitle(WebDriver driver,String title){
+        Set<String> allPagesId =driver.getWindowHandles();
+        for(String id:allPagesId){
+            driver.switchTo().window(id);
+            if(driver.getTitle().contains(title)){
+                break;
+            }
+        }
+}
+public static void getScreenShot(WebDriver driver,String packageName){
+        File file =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String location = System.getProperty("st/test/java/"+packageName+"/");
+    try {
+        FileUtils.copyFile(file,new File(location+System.currentTimeMillis()));
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+
+}
+    public static void getScreenShotForCucumber(WebDriver driver, Scenario scenario){
+        Date currentDate=new Date();
+        String screenShotFileName=currentDate.toString().replace(" ","-").
+                replace(":","-");
+        if(scenario.isFailed()){
+            File screenShotFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(screenShotFile,new File("src/test/java/screenshot/"+screenShotFileName+".png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
